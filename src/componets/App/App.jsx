b7c7ed668aae7ef12738d0bd16586461c6294597
windofbloom <bloomillus@gmail.com>
2024-07-28 21:15:35 -0500

    Delete not useful files, organize basic folders and add general css

---
+++ src/componets/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App_container'>
+      <h1>Pomodoro</h1>
+      <small>Be productive the right way</small>
+    </div>
+  )
+}
+
+export default App
